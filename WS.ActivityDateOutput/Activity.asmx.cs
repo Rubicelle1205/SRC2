4c6189267ad2b5ff196a4aa3416adc27bd1626e4
Rubicelle1205 <Rubicelle1205@gmail.com>
2023-04-30 15:45:19 +0800

    調整WS 相關判斷
    Signed-off-by: Rubicelle1205 <Rubicelle1205@gmail.com>

--- WS.ActivityDateOutput/Activity.asmx.cs
+++ WS.ActivityDateOutput/Activity.asmx.cs
@@ -1,6 +1,9 @@
 ﻿using System.Web.Services;
 using System.Data;
 using Newtonsoft.Json;
+using System;
+using WebPccuClub.Models;
+using System.Security.Policy;
 
 namespace WebPccuClub.WS.ActivityDateOuput
 {
@@ -14,9 +17,16 @@ namespace WebPccuClub.WS.ActivityDateOuput
     // [System.Web.Script.Services.ScriptService]
     public class Activity : System.Web.Services.WebService
     {
+        ReturnViewModel vmRtn = new ReturnViewModel();
+
         [WebMethod]
         public string GetActivityData(string SDate, string EDate)
         {
+            string chkStr = ChkDate(SDate, EDate);
+
+            if (chkStr != "")
+                return chkStr;
+
             DataTable dt = new DataTable();
 
             dt.Columns.Add("SpaceID");
@@ -34,6 +44,39 @@ namespace WebPccuClub.WS.ActivityDateOuput
             }
 
             return JsonConvert.SerializeObject(dt);
+        }
+
+        private string ChkDate(string SDate, string EDate)
+        {
+            DateTime d1 = new DateTime();
+            DateTime d2 = new DateTime();
+            string f1 = SDate.Length == 8 ? DateTime.TryParse(string.Format("{0}-{1}-{2}", SDate.Substring(0, 4), SDate.Substring(4, 2), SDate.Substring(6, 2)), out d1) ? string.Format("{0}-{1}-{2}", SDate.Substring(0, 4), SDate.Substring(4, 2), SDate.Substring(6, 2)) : "error" : "error";
+            string f2 = EDate.Length == 8 ? DateTime.TryParse(string.Format("{0}-{1}-{2}", EDate.Substring(0, 4), EDate.Substring(4, 2), EDate.Substring(6, 2)), out d2) ? string.Format("{0}-{1}-{2}", EDate.Substring(0, 4), EDate.Substring(4, 2), EDate.Substring(6, 2)) : "error" : "error";
+            string f3 = f1 != "error" && f2 != "error" ? GetDay(f1, f2) : "error";
+            if (f1 == "error" || f2 == "error")
+            {
+                vmRtn.ErrorCode = 1;
+                vmRtn.ErrorMsg = "錯誤的日期輸入";
+                return JsonConvert.SerializeObject(vmRtn);
+            }
+            
+            if (f3 != "ok")
+            {
+                vmRtn.ErrorCode = 1;
+                vmRtn.ErrorMsg = "輸入期間超過90天限制";
+                return JsonConvert.SerializeObject(vmRtn);
+            }
+            return "";
+        }
+        private static string GetDay(string f1, string f2)
+        {
+            TimeSpan ts = DateTime.Parse(f1) - DateTime.Parse(f2);
+            int diff = Int32.Parse(ts.ToString("dd"));
+
+            if (diff > 90)
+                return "error";
+            else
+                return "ok";
         }
     }
 }
---
+++ WS.ActivityDateOutput/RetrunViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace WebPccuClub.Models
+{
+    public class ReturnViewModel
+    {
+        public ReturnViewModel() {
+            ErrorCode = 0;
+            ErrorMsg = "";
+        }
+
+        public int ErrorCode { get; set; }
+
+        public string ErrorMsg { get; set; }
+    }
+}
