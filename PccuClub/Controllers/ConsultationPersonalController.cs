2e08f8ab807f75bd7bb95cb3d9309c9b1d992e4c
Rubicelle <Rubicelle@gmail.com>
2024-04-15 10:31:03 +0800

    [輔導諮商] 增加輔導諮商-個人資料完成
    Signed-off-by: Rubicelle <Rubicelle@gmail.com>

---
+++ PccuClub/Controllers/ConsultationPersonalController.cs
@@ -0,0 +1,99 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using PccuClub.WebAuth;
+using System.Web.Mvc;
+using WebPccuClub.DataAccess;
+using WebPccuClub.Global;
+using WebPccuClub.Models;
+
+namespace WebPccuClub.Controllers
+{
+    [LogAttribute(LogActionChineseName.個人資料)]
+    public class ConsultationPersonalController : BaseController
+    {
+        ReturnViewModel vmRtn = new ReturnViewModel();
+        ConsultationPersonalDataAccess dbAccess = new ConsultationPersonalDataAccess();
+
+        [Log(LogActionChineseName.首頁)]
+        public IActionResult Index()
+        {
+            ConsultationPersonalViewModel vm = new ConsultationPersonalViewModel();
+            vm.EditModel = dbAccess.GetSearchResult(LoginUser.LoginId).FirstOrDefault();
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.編輯儲存)]
+        public IActionResult EditOldData(ConsultationPersonalViewModel vm)
+        {
+            AuthManager auth = new AuthManager();
+            string strErr = string.Empty;
+
+            if (!ChkData(vm.EditModel, out strErr))
+            {
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = string.Format(@"{0}", strErr);
+                return Json(vmRtn);
+            }
+
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+                string EncryptPw = String.Empty;
+                
+                if (!string.IsNullOrEmpty(vm.EditModel.Pwd))
+                    EncryptPw = auth.EncryptionText(vm.EditModel.Pwd);
+
+                var dbResult = dbAccess.UpdateConsultationPersonalData(EncryptPw, vm.EditModel, LoginUser);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = string.Format(@"更新失敗:{0}", dbResult.ErrorMessage);
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = string.Format(@"更新失敗:{0}", ex.Message);
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        private bool ChkData(ConsultationPersonalEditModel editModel, out string msg)
+        {
+            msg = string.Empty;
+
+            if (!string.IsNullOrEmpty(editModel.Pwd))
+            {
+                if (editModel.Pwd != editModel.ConformPwd)
+                    msg += "請確認密碼與密碼確認輸入的資料相等!<br/>";
+
+                if (6 > editModel.Pwd.Length || 15 < editModel.Pwd.Length)
+                    msg += "密碼長度錯誤!<br/>";
+
+                if (!editModel.Pwd.HasNumber())
+                    msg += "密碼需包含至少一個數字!<br/>";
+
+                if (!editModel.Pwd.HasUpperText() && !editModel.Pwd.HasLowerText())
+                    msg += "至少包含一大寫或小寫英文字母!<br/>";
+            }
+
+            if (!string.IsNullOrEmpty(editModel.EMail))
+            {
+                if (!editModel.EMail.Contains("@"))
+                {
+                    msg += "信箱格式錯誤!<br/>";
+                }
+            }
+
+            return msg == "";
+        }
+    }
+}
---
+++ PccuClub/DataAccess/ConsultationPersonalDataAccess.cs
@@ -0,0 +1,81 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Components.Forms;
+using NPOI.POIFS.Crypt;
+using PccuClub.WebAuth;
+using System.Data;
+using System.Text.Encodings.Web;
+using WebPccuClub.Global;
+using WebPccuClub.Models;
+
+namespace WebPccuClub.DataAccess
+{
+    public class ConsultationPersonalDataAccess : BaseAccess
+    {
+        public List<ConsultationPersonalEditModel> GetSearchResult(string LoginId)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+            #region 參數設定
+
+            parameters.Add("@LoginId", LoginId);
+
+            #endregion
+
+            CommandText = $@"SELECT LoginId, UserName, EMail, Creator, Created, LastModifier, LastModified
+                               FROM UserMain
+                              WHERE LoginId = @LoginId";
+
+            (DbExecuteInfo info, IEnumerable<ConsultationPersonalEditModel> entitys) dbResult = DbaExecuteQuery<ConsultationPersonalEditModel>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<ConsultationPersonalEditModel>();
+        }
+
+        public DbExecuteInfo UpdateConsultationPersonalData(string EncryptPw, ConsultationPersonalEditModel editModel, UserInfo LoginUser)
+        {
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+            string CommendText = string.Empty;
+
+            if (!string.IsNullOrEmpty(EncryptPw))
+            {
+                #region 參數設定
+                parameters.Add("@Password", EncryptPw);
+                parameters.Add("@EMail", editModel.EMail);
+                parameters.Add("@LastModifier", LoginUser.UserName);
+                parameters.Add("@LoginId", LoginUser.LoginId);
+                #endregion 參數設定
+
+                CommendText = $@"UPDATE UserMain 
+                                       SET Password = @Password,
+                                           EMail = @EMail,
+                                           LastModified = GETDATE(),
+                                           LastModifier = @LastModifier
+                                     WHERE LoginId = @LoginId";
+            }
+            else
+            {
+                #region 參數設定
+                parameters.Add("@EMail", editModel.EMail);
+                parameters.Add("@LastModifier", LoginUser.UserName);
+                parameters.Add("@LoginId", LoginUser.LoginId);
+                #endregion 參數設定
+
+                CommendText = $@"UPDATE UserMain 
+                                       SET EMail = @EMail,
+                                           LastModified = GETDATE(),
+                                           LastModifier = @LastModifier
+                                     WHERE LoginId = @LoginId";
+            }
+            
+
+            ExecuteResult = DbaExecuteNonQuery(CommendText, parameters, false, DBAccessException);
+
+            return ExecuteResult;
+        }
+    }
+}
