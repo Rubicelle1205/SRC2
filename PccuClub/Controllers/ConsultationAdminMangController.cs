8fbff32be335306c510811d26f61d316523aae20
Rubicelle <Rubicelle@gmail.com>
2024-04-15 14:30:37 +0800

    [輔導諮商] 管理員維護第一次上版(未完成)
    Signed-off-by: Rubicelle <Rubicelle@gmail.com>

---
+++ PccuClub/Controllers/ConsultationAdminMangController.cs
@@ -0,0 +1,237 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using NPOI.POIFS.Crypt;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using PccuClub.WebAuth;
+using System.ComponentModel;
+using System.Reflection;
+using System.Web.Mvc;
+using Utility;
+using WebAuth.Entity;
+using WebPccuClub.DataAccess;
+using WebPccuClub.Global;
+using WebPccuClub.Models;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace WebPccuClub.Controllers
+{
+    [LogAttribute(LogActionChineseName.管理員維護)]
+    public class ConsultationAdminMangController : BaseController
+    {
+        ReturnViewModel vmRtn = new ReturnViewModel();
+        ConsultationAdminMangDataAccess dbAccess = new ConsultationAdminMangDataAccess();
+        AuthManager auth = new AuthManager();
+
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public ConsultationAdminMangController(IHostingEnvironment _hostingEnvironment)
+        {
+            hostingEnvironment = _hostingEnvironment;
+        }
+
+
+        [Log(LogActionChineseName.首頁)]
+        public IActionResult Index()
+        {
+            ViewBag.ddlRole = dbAccess.GetRoleData("05");
+            ViewBag.ddlIsEnable = dbAccess.GetIsEnable();
+            ViewBag.ddlLifeClass = dbAccess.GetAllLifeClass();
+
+            ConsultationAdminMangViewModel vm = new ConsultationAdminMangViewModel();
+            vm.ConditionModel = new ConsultationAdminMangConditionModel();
+
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.新增)]
+        public IActionResult Create()
+        {
+            ViewBag.ddlIsEnable = dbAccess.GetIsEnable();
+            ViewBag.ddlYesOrNo = dbAccess.GetYesOrNo();
+
+            ViewBag.ddlRoleConsultation = dbAccess.GetRoleData("05");
+
+            ConsultationAdminMangViewModel vm = new ConsultationAdminMangViewModel();
+            vm.CreateModel = new ConsultationAdminMangCreateModel();
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.編輯)]
+        public IActionResult Edit(string submitBtn, ConsultationAdminMangViewModel vm)
+        {
+            if (string.IsNullOrEmpty(submitBtn))
+                return RedirectToAction("Index");
+
+            ViewBag.ddlIsEnable = dbAccess.GetIsEnable();
+            ViewBag.ddlYesOrNo = dbAccess.GetYesOrNo();
+
+            ViewBag.ddlRoleConsultation = dbAccess.GetRoleData("05");
+
+            vm.EditModel = dbAccess.GetEditData(submitBtn);
+            vm.EditModel.LstCanUseFun = LoginUser.UserRoleFun.Where(x => !string.IsNullOrEmpty(x.SystemCode)).Select(x => x.SystemCode).Distinct().ToList();
+
+            return View(vm);
+        }
+
+        [LogAttribute(LogActionChineseName.查詢)]
+        public IActionResult GetSearchResult(ConsultationAdminMangViewModel vm)
+        {
+            vm.ResultModel = dbAccess.GetSearchResult(vm.ConditionModel).ToList();
+
+            #region 分頁
+            vm.ConditionModel.TotalCount = vm.ResultModel.Count();
+            int StartRow = vm.ConditionModel.Page * vm.ConditionModel.PageSize;
+            vm.ResultModel = vm.ResultModel.Skip(StartRow).Take(vm.ConditionModel.PageSize).ToList();
+            #endregion
+
+            return PartialView("_SearchResultPartial", vm);
+        }
+
+        [Log(LogActionChineseName.新增儲存)]
+        [ValidateInput(false)]
+        public IActionResult SaveNewData(ConsultationAdminMangViewModel vm)
+        {
+            try
+            {
+                vm.EditModel = dbAccess.GetEditData(vm.CreateModel.LoginId);
+
+                if (vm.EditModel != null)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = String.Format("管理者帳號:{0}已存在", vm.CreateModel.LoginId);
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaInitialTransaction();
+
+                string EncryptPw = String.Empty;
+                if (!string.IsNullOrEmpty(vm.CreateModel.Pwd))
+                    EncryptPw = auth.EncryptionText(vm.CreateModel.Pwd);
+
+                var dbResult = dbAccess.InsertData(vm, LoginUser, EncryptPw);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "新增失敗";
+                    return Json(vmRtn);
+                }
+
+                dbResult = dbAccess.InsertRole(vm, LoginUser);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "新增失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "新增失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        [Log(LogActionChineseName.編輯儲存)]
+        [ValidateInput(false)]
+        public IActionResult EditOldData(ConsultationAdminMangViewModel vm)
+        {
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+
+                string EncryptPw = String.Empty;
+                if (!string.IsNullOrEmpty(vm.EditModel.Pwd))
+                    EncryptPw = auth.EncryptionText(vm.EditModel.Pwd);
+
+                var dbResult = dbAccess.UpdateData(vm, LoginUser, EncryptPw);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "修改失敗";
+                    return Json(vmRtn);
+                }
+
+                dbResult = dbAccess.UpdateRole(vm, LoginUser);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "修改失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "修改失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        [Log(LogActionChineseName.刪除)]
+        [ValidateInput(false)]
+        public IActionResult Delete(string Ser)
+        {
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+
+                var dbResult = dbAccess.DeleteRole(Ser);
+
+                if (!dbResult.isSuccess) 
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode =  (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "刪除失敗";
+                    return Json(vmRtn);
+                }
+
+                dbResult = dbAccess.DeletetData(Ser);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "刪除失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "刪除失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        #region Method
+
+        #endregion
+    }
+}
---
+++ PccuClub/Models/ConsultationAdminMangViewModel.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using NPOI.SS.Formula.Functions;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using WebAuth.Entity;
+using WebPccuClub.Global.Extension;
+
+namespace WebPccuClub.Models
+{
+    public class ConsultationAdminMangViewModel
+    {
+        [DisplayName("檔案上傳")]
+        [UIHint("_UploadFile")]
+        public IFormFile? File { get; set; }
+
+        public ConsultationAdminMangConditionModel ConditionModel { get; set; }
+
+        public List<ConsultationAdminMangResultModel> ResultModel { get; set; }
+
+        public ConsultationAdminMangCreateModel CreateModel { get; set; }
+
+        public ConsultationAdminMangEditModel EditModel { get; set; }
+    }
+
+    public class ConsultationAdminMangConditionModel
+    {
+        public ConsultationAdminMangConditionModel()
+        {
+            this.Page = 0;
+            this.PageSize = 10;
+            this.TotalCount = 0;
+        }
+
+        /// <summary> 目前頁數 </summary>
+        public int Page { get; set; }
+
+        /// <summary> 預設每頁顯示筆數 - 依需求更改 </summary>
+        public int PageSize { get; set; }
+
+        /// <summary> 總筆數 </summary>
+        public int TotalCount { get; set; }
+
+        /// <summary>管理員帳號</summary>
+        [DisplayName("管理員帳號")]
+        public string? LoginId { get; set; }
+
+        /// <summary>管理員名稱</summary>
+        [DisplayName("管理員名稱")]
+        public string? UserName { get; set; }
+
+        /// <summary>角色ID</summary>
+        [DisplayName("角色ID")]
+        public string? RoleId { get; set; }
+
+        /// <summary>啟用</summary>
+        [DisplayName("啟用")]
+        public string IsEnable { get; set; }
+
+        [DisplayName("起始日期")]
+        public DateTime? From_ReleaseDate { get; set; }
+
+        [DisplayName("結束日期")]
+        public DateTime? To_ReleaseDate { get; set; }
+    }
+
+    public class ConsultationAdminMangResultModel
+    {
+        /// <summary>管理員帳號</summary>
+        [DisplayName("管理員帳號")]
+        public string? LoginId { get; set; }
+
+        /// <summary>管理員名稱</summary>
+        [DisplayName("管理員名稱")]
+        public string? UserName { get; set; }
+
+        /// <summary>角色ID</summary>
+        [DisplayName("角色ID")]
+        public string? RoleId { get; set; }
+
+        /// <summary>角色名稱</summary>
+        [DisplayName("角色名稱")]
+        public string? RoleName { get; set; }
+
+        /// <summary>啟用</summary>
+        [DisplayName("啟用")]
+        public string? EnableText { get; set; }
+
+        /// <summary>備註</summary>
+        [DisplayName("備註")]
+        public string? Memo { get; set; }
+
+        [DisplayName("更新時間")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}")]
+        public DateTime? LastModified { get; set; }
+
+    }
+
+    public class ConsultationAdminMangCreateModel
+    {
+        public List<string> LstCanUseFun = new List<string>();
+
+        [DisplayName("建立時間")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}")]
+        public DateTime? Created { get; set; }
+
+        [DisplayName("更新時間")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}")]
+        public DateTime? LastModified { get; set; }
+
+        /// <summary>管理員帳號</summary>
+        [DisplayName("管理員帳號")]
+        public string? LoginId { get; set; }
+
+        /// <summary>密碼</summary>
+        [DisplayName("密碼")]
+        public string? Pwd { get; set; }
+
+        /// <summary>密碼</summary>
+        [DisplayName("密碼")]
+        public string? ConformPwd { get; set; }
+
+        /// <summary>管理員名稱</summary>
+        [DisplayName("管理員名稱")]
+        public string? UserName { get; set; }
+
+        /// <summary>啟用</summary>
+        [DisplayName("啟用")]
+        public string? IsEnable { get; set; }
+
+        /// <summary>角色-諮商系統</summary>
+        [DisplayName("角色-諮商系統")]
+        public string? RoleConsultation { get; set; }
+
+        /// <summary>信箱</summary>
+        [DisplayName("信箱")]
+        public string? EMail { get; set; }
+
+        /// <summary>單位</summary>
+        [DisplayName("單位")]
+        public string? Department { get; set; }
+
+        /// <summary>啟用</summary>
+        [DisplayName("啟用")]
+        public string? EnableText { get; set; }
+
+        /// <summary>備註</summary>
+        [DisplayName("備註")]
+        public string? Memo { get; set; }
+    }
+
+    public class ConsultationAdminMangEditModel
+    {
+        public List<string> LstCanUseFun = new List<string>();
+
+        [DisplayName("建立時間")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}")]
+        public DateTime? Created { get; set; }
+
+        [DisplayName("更新時間")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}")]
+        public DateTime? LastModified { get; set; }
+
+        /// <summary>管理員帳號</summary>
+        [DisplayName("管理員帳號")]
+        public string? LoginId { get; set; }
+
+        /// <summary>密碼</summary>
+        [DisplayName("密碼")]
+        public string? Pwd { get; set; }
+
+        /// <summary>密碼</summary>
+        [DisplayName("密碼")]
+        public string? ConformPwd { get; set; }
+
+        /// <summary>管理員名稱</summary>
+        [DisplayName("管理員名稱")]
+        public string? UserName { get; set; }
+
+        /// <summary>啟用</summary>
+        [DisplayName("啟用")]
+        public string? IsEnable { get; set; }
+
+        /// <summary>使用者類型</summary>
+        [DisplayName("使用者類型")]
+        public string? UserType { get; set; }
+
+        /// <summary>角色-社團系統</summary>
+        [DisplayName("角色-社團系統")]
+        public string? RoleClub { get; set; }
+
+        /// <summary>角色-案件系統</summary>
+        [DisplayName("角色-案件系統")]
+        public string? RoleCase { get; set; }
+
+        /// <summary>角色-借用系統</summary>
+        [DisplayName("角色-借用系統")]
+        public string? RoleBorrow { get; set; }
+
+        /// <summary>角色-諮商系統</summary>
+        [DisplayName("角色-諮商系統")]
+        public string? RoleConsultation { get; set; }
+
+        ///// <summary>角色</summary>
+        //[DisplayName("角色")]
+        //public string? RoleId { get; set; }
+
+        /// <summary>信箱</summary>
+        [DisplayName("信箱")]
+        public string? EMail { get; set; }
+
+        /// <summary>單位</summary>
+        [DisplayName("單位")]
+        public string? Department { get; set; }
+
+        /// <summary>啟用</summary>
+        [DisplayName("啟用")]
+        public string? EnableText { get; set; }
+
+        /// <summary>備註</summary>
+        [DisplayName("備註")]
+        public string? Memo { get; set; }
+    }
+}
