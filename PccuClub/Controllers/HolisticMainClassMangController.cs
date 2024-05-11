87d567f3c6bed9dbabf66a79e912dc9d7ea56f9b
Rubicelle <Rubicelle@gmail.com>
2024-05-11 22:23:15 +0800

    [社團] 全人群組管理完成
    Signed-off-by: Rubicelle <Rubicelle@gmail.com>

---
+++ PccuClub/Controllers/HolisticMainClassMangController.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Web.Mvc;
+using Utility;
+using WebPccuClub.DataAccess;
+using WebPccuClub.Global;
+using WebPccuClub.Models;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace WebPccuClub.Controllers
+{
+    [LogAttribute(LogActionChineseName.全人群組管理)]
+    public class HolisticMainClassMangController : BaseController
+    {
+        ReturnViewModel vmRtn = new ReturnViewModel();
+        HolisticMainClassMangDataAccess dbAccess = new HolisticMainClassMangDataAccess();
+
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public HolisticMainClassMangController(IHostingEnvironment _hostingEnvironment)
+        {
+            hostingEnvironment = _hostingEnvironment;
+        }
+
+
+        [Log(LogActionChineseName.首頁)]
+        public IActionResult Index()
+        {
+            HolisticMainClassMangViewModel vm = new HolisticMainClassMangViewModel();
+            vm.ConditionModel = new HolisticMainClassMangConditionModel();
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.新增)]
+        public IActionResult Create()
+        {
+            HolisticMainClassMangViewModel vm = new HolisticMainClassMangViewModel();
+            vm.CreateModel = new HolisticMainClassMangCreateModel();
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.編輯)]
+        public IActionResult Edit(string submitBtn, HolisticMainClassMangViewModel vm)
+        {
+            if (string.IsNullOrEmpty(submitBtn))
+                return RedirectToAction("Index");
+
+            //HolisticMainClassMangViewModel vm = new HolisticMainClassMangViewModel();
+            vm.EditModel = dbAccess.GetEditData(submitBtn);
+            return View(vm);
+        }
+
+        [LogAttribute(LogActionChineseName.查詢)]
+        public IActionResult GetSearchResult(HolisticMainClassMangViewModel vm)
+        {
+            vm.ResultModel = dbAccess.GetSearchResult(vm.ConditionModel).ToList();
+
+            #region 分頁
+            vm.ConditionModel.TotalCount = vm.ResultModel.Count();
+            int StartRow = vm.ConditionModel.Page * vm.ConditionModel.PageSize;
+            vm.ResultModel = vm.ResultModel.Skip(StartRow).Take(vm.ConditionModel.PageSize).ToList();
+            #endregion
+
+            return PartialView("_SearchResultPartial", vm);
+        }
+
+        [Log(LogActionChineseName.新增儲存)]
+        [ValidateInput(false)]
+        public IActionResult SaveNewData(HolisticMainClassMangViewModel vm)
+        {
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+
+                var dbResult = dbAccess.InsertData(vm, LoginUser);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "新增失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "新增失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        [Log(LogActionChineseName.編輯儲存)]
+        [ValidateInput(false)]
+        public IActionResult EditOldData(HolisticMainClassMangViewModel vm)
+        {
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+
+                var dbResult = dbAccess.UpdateData(vm, LoginUser);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "修改失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "修改失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        [Log(LogActionChineseName.刪除)]
+        [ValidateInput(false)]
+        public IActionResult Delete(string Ser)
+        {
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+
+                var dbResult = dbAccess.DeletetData(Ser);
+
+                if (!dbResult.isSuccess)
+                {
+                    vmRtn.ErrorCode =  (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "刪除失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "刪除失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+    }
+}
