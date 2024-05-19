a87fad16923e175846165025982a4ca713df7e2e
Rubicelle <Rubicelle@gmail.com>
2024-05-19 12:35:25 +0800

    [借用]業務子分類維護上傳完成
    Signed-off-by: Rubicelle <Rubicelle@gmail.com>

---
+++ PccuClub/Controllers/BorrowSecondClassMangController.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Web.Mvc;
+using Utility;
+using WebPccuClub.DataAccess;
+using WebPccuClub.Global;
+using WebPccuClub.Models;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace WebPccuClub.Controllers
+{
+    [LogAttribute(LogActionChineseName.業務子分類維護)]
+    public class BorrowSecondClassMangController : BaseController
+    {
+        ReturnViewModel vmRtn = new ReturnViewModel();
+        BorrowSecondClassMangDataAccess dbAccess = new BorrowSecondClassMangDataAccess();
+
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public BorrowSecondClassMangController(IHostingEnvironment _hostingEnvironment)
+        {
+            hostingEnvironment = _hostingEnvironment;
+        }
+
+
+        [Log(LogActionChineseName.首頁)]
+        public IActionResult Index()
+        {
+            ViewBag.ddlMainClass = dbAccess.GetddlMainClass();
+
+            BorrowSecondClassMangViewModel vm = new BorrowSecondClassMangViewModel();
+            vm.ConditionModel = new BorrowSecondClassMangConditionModel();
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.新增)]
+        public IActionResult Create()
+        {
+            ViewBag.ddlMainClass = dbAccess.GetddlMainClass();
+
+            BorrowSecondClassMangViewModel vm = new BorrowSecondClassMangViewModel();
+            vm.CreateModel = new BorrowSecondClassMangCreateModel();
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.編輯)]
+        public IActionResult Edit(string submitBtn, BorrowSecondClassMangViewModel vm)
+        {
+            if (string.IsNullOrEmpty(submitBtn))
+                return RedirectToAction("Index");
+
+            ViewBag.ddlMainClass = dbAccess.GetddlMainClass();
+
+            //BorrowSecondClassMangViewModel vm = new BorrowSecondClassMangViewModel();
+            vm.EditModel = dbAccess.GetEditData(submitBtn);
+            return View(vm);
+        }
+
+        [LogAttribute(LogActionChineseName.查詢)]
+        public IActionResult GetSearchResult(BorrowSecondClassMangViewModel vm)
+        {
+            vm.ResultModel = dbAccess.GetSearchResult(vm.ConditionModel).ToList();
+
+            #region 分頁
+            vm.ConditionModel.TotalCount = vm.ResultModel.Count();
+            int StartRow = vm.ConditionModel.Page * vm.ConditionModel.PageSize;
+            vm.ResultModel = vm.ResultModel.Skip(StartRow).Take(vm.ConditionModel.PageSize).ToList();
+            #endregion
+
+            return PartialView("_SearchResultPartial", vm);
+        }
+
+        [Log(LogActionChineseName.新增儲存)]
+        [ValidateInput(false)]
+        public IActionResult SaveNewData(BorrowSecondClassMangViewModel vm)
+        {
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+
+                var dbResult = dbAccess.InsertData(vm, LoginUser);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "新增失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "新增失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        [Log(LogActionChineseName.編輯儲存)]
+        [ValidateInput(false)]
+        public IActionResult EditOldData(BorrowSecondClassMangViewModel vm)
+        {
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+
+                var dbResult = dbAccess.UpdateData(vm, LoginUser);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "修改失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "修改失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        [Log(LogActionChineseName.刪除)]
+        [ValidateInput(false)]
+        public IActionResult Delete(string Ser)
+        {
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+
+                var dbResult = dbAccess.DeletetData(Ser);
+
+                if (!dbResult.isSuccess)
+                {
+                    vmRtn.ErrorCode =  (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "刪除失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "刪除失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+    }
+}
---
+++ PccuClub/DataAccess/BorrowSecondClassMangDataAccess.cs
@@ -0,0 +1,186 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NPOI.POIFS.Crypt;
+using PccuClub.WebAuth;
+using System.Data;
+using System.Text.Encodings.Web;
+using WebPccuClub.Global;
+using WebPccuClub.Global.Extension;
+using WebPccuClub.Models;
+
+namespace WebPccuClub.DataAccess
+{
+    
+    public class BorrowSecondClassMangDataAccess : BaseAccess
+    {
+
+        /// <summary> 查詢結果 </summary>
+
+        public List<BorrowSecondClassMangResultModel> GetSearchResult(BorrowSecondClassMangConditionModel model)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            parameters.Add("@MainClass", model?.MainClass);
+            parameters.Add("@Text", model?.Text);
+            parameters.Add("@Memo", model?.Memo);
+
+            #region 參數設定
+            #endregion
+
+            CommandText = $@"
+SELECT A.ID, A.Text, A.MainClass, B.Text AS MainClassText, A.Memo, A.Creator, A.Created, A.LastModifier, A.LastModified
+FROM BorrowSecondClassMang A
+LEFT JOIN BorrowMainClassMang B ON B.ID = A.MainClass
+WHERE 1 = 1
+AND (@MainClass IS NULL OR A.MainClass = @MainClass) 
+AND (@Text IS NULL OR A.Text LIKE '%' + @Text + '%') 
+AND (@Memo IS NULL OR A.Memo LIKE '%' + @Memo + '%') ";
+
+
+            (DbExecuteInfo info, IEnumerable<BorrowSecondClassMangResultModel> entitys) dbResult = DbaExecuteQuery<BorrowSecondClassMangResultModel>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<BorrowSecondClassMangResultModel>();
+        }
+
+        /// <summary>
+        /// 取得編輯資料
+        /// </summary>
+        /// <param name="submitBtn"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public BorrowSecondClassMangEditModel GetEditData(string Ser)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            parameters.Add("@ID", Ser);
+
+            #region 參數設定
+            #endregion
+
+            CommandText = $@"
+SELECT ID, Text, MainClass, Memo, Creator, Created, LastModifier, LastModified
+FROM BorrowSecondClassMang
+WHERE 1 = 1
+AND (ID = @ID) ";
+
+
+            (DbExecuteInfo info, IEnumerable<BorrowSecondClassMangEditModel> entitys) dbResult = DbaExecuteQuery<BorrowSecondClassMangEditModel>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList().FirstOrDefault();
+
+            return null;
+        }
+
+        /// <summary> 新增資料 </summary>
+        public DbExecuteInfo InsertData(BorrowSecondClassMangViewModel vm, UserInfo LoginUser)
+        {
+
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@Text", vm.CreateModel.Text);
+            parameters.Add("@MainClass", vm.CreateModel.MainClass);
+            parameters.Add("@Memo", vm.CreateModel.Memo);
+            parameters.Add("@LoginId", LoginUser.LoginId);
+            #endregion 參數設定
+
+            string CommendText = $@"INSERT INTO BorrowSecondClassMang
+                                               (Text
+                                               ,MainClass
+                                               ,Memo
+                                               ,Creator
+                                               ,Created
+                                               ,LastModifier
+                                               ,LastModified)
+                                         VALUES
+                                               (@Text
+                                               ,@MainClass
+                                               ,@Memo
+                                               ,@LoginId
+                                               ,GETDATE()
+                                               ,@LoginId
+                                               ,GETDATE())";
+
+            ExecuteResult = DbaExecuteNonQuery(CommendText, parameters, false, DBAccessException);
+
+            return ExecuteResult;
+        }
+
+        /// <summary> 修改資料 </summary>
+        public DbExecuteInfo UpdateData(BorrowSecondClassMangViewModel vm, UserInfo LoginUser)
+        {
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@ID", vm.EditModel.ID);
+            parameters.Add("@MainClass", vm.EditModel.MainClass);
+            parameters.Add("@Memo", vm.EditModel.Memo);
+            parameters.Add("@Text", vm.EditModel.Text);
+            parameters.Add("@LoginId", LoginUser.LoginId);
+            #endregion 參數設定
+
+            string CommendText = $@"UPDATE BorrowSecondClassMang 
+                                       SET Text = @Text, MainClass = @MainClass , Memo = @Memo, LastModifier = @LoginId, LastModified = GETDATE()
+                                     WHERE ID = @ID";
+
+            ExecuteResult = DbaExecuteNonQuery(CommendText, parameters, false, DBAccessException);
+
+            return ExecuteResult;
+        }
+
+        /// <summary>
+        /// 刪除資料
+        /// </summary>
+        /// <param name="ser"></param>
+        /// <returns></returns>
+        public DbExecuteInfo DeletetData(string ser)
+        {
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@ID", ser);
+            #endregion 參數設定
+
+            string CommendText = $@"DELETE FROM BorrowSecondClassMang WHERE ID = @ID ";
+
+            ExecuteResult = DbaExecuteNonQuery(CommendText, parameters, false, DBAccessException);
+
+            return ExecuteResult;
+        }
+
+        public List<SelectListItem> GetddlMainClass()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT ID AS Value, Text AS Text FROM BorrowMainClassMang";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+
+    }
+}
