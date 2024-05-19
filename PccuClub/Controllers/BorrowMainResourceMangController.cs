4241f4ca7b0fd083e36a60bfba7b5c4ef92591ba
Rubicelle <Rubicelle@gmail.com>
2024-05-19 16:57:53 +0800

    暫時上傳
    Signed-off-by: Rubicelle <Rubicelle@gmail.com>

---
+++ PccuClub/Controllers/BorrowMainResourceMangController.cs
@@ -0,0 +1,231 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.ComponentModel;
+using System.Data;
+using System.Reflection;
+using System.Web.Mvc;
+using Utility;
+using WebPccuClub.DataAccess;
+using WebPccuClub.Global;
+using WebPccuClub.Models;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace WebPccuClub.Controllers
+{
+    [LogAttribute(LogActionChineseName.主資源維護)]
+    public class BorrowMainResourceMangController : BaseController
+    {
+        ReturnViewModel vmRtn = new ReturnViewModel();
+        BorrowMainResourceMangDataAccess dbAccess = new BorrowMainResourceMangDataAccess();
+        UploadUtil upload = new UploadUtil();
+
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public BorrowMainResourceMangController(IHostingEnvironment _hostingEnvironment)
+        {
+            hostingEnvironment = _hostingEnvironment;
+        }
+
+
+        [Log(LogActionChineseName.首頁)]
+        public IActionResult Index()
+        {
+            ViewBag.ddlMainClass = dbAccess.GetddlMainClass();
+            ViewBag.ddlEnable = dbAccess.GetddlEnable();
+
+
+            BorrowMainResourceMangViewModel vm = new BorrowMainResourceMangViewModel();
+            vm.ConditionModel = new BorrowMainResourceMangConditionModel();
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.新增)]
+        public IActionResult Create()
+        {
+            ViewBag.ddlMainClass = dbAccess.GetddlMainClass();
+            ViewBag.ddlSecondClass = dbAccess.GetddlSecondClass();
+            ViewBag.ddlEnable = dbAccess.GetddlEnable();
+            ViewBag.ddlBorrowMultType = dbAccess.GetddlBorrowMultType();
+
+            BorrowMainResourceMangViewModel vm = new BorrowMainResourceMangViewModel();
+            vm.CreateModel = new BorrowMainResourceMangCreateModel();
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.編輯)]
+        public IActionResult Edit(string submitBtn, BorrowMainResourceMangViewModel vm)
+        {
+            if (string.IsNullOrEmpty(submitBtn))
+                return RedirectToAction("Index");
+
+            ViewBag.ddlMainClass = dbAccess.GetddlMainClass();
+            ViewBag.ddlSecondClass = dbAccess.GetddlSecondClass();
+            ViewBag.ddlEnable = dbAccess.GetddlEnable();
+            ViewBag.ddlBorrowMultType = dbAccess.GetddlBorrowMultType();
+
+            //BorrowMainResourceMangViewModel vm = new BorrowMainResourceMangViewModel();
+            vm.EditModel = dbAccess.GetEditData(submitBtn);
+            return View(vm);
+        }
+
+        [LogAttribute(LogActionChineseName.查詢)]
+        public IActionResult GetSearchResult(BorrowMainResourceMangViewModel vm)
+        {
+            vm.ResultModel = dbAccess.GetSearchResult(vm.ConditionModel).ToList();
+
+            #region 分頁
+            vm.ConditionModel.TotalCount = vm.ResultModel.Count();
+            int StartRow = vm.ConditionModel.Page * vm.ConditionModel.PageSize;
+            vm.ResultModel = vm.ResultModel.Skip(StartRow).Take(vm.ConditionModel.PageSize).ToList();
+            #endregion
+
+            return PartialView("_SearchResultPartial", vm);
+        }
+
+        [Log(LogActionChineseName.新增儲存)]
+        [ValidateInput(false)]
+        public async Task<IActionResult> SaveNewDataAsync(BorrowMainResourceMangViewModel vm)
+        {
+            try
+            {
+
+                DataTable dt = dbAccess.GetMainResourceID(vm.CreateModel.MainResourceID);
+                string ResourceID = dt.QueryFieldByDT("MainResourceID");
+
+                if (!string.IsNullOrEmpty(ResourceID)) 
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = $@"新增失敗， 資源代碼 {ResourceID} 已存在";
+                    return Json(vmRtn);
+                }
+
+
+                dbAccess.DbaInitialTransaction();
+
+                if (Request.Form.Files.Count > 0)
+                {
+                    for (int i = 0; i <= Request.Form.Files.Count - 1; i++)
+                    {
+                        if (Request.Form.Files[i].Name.Contains("ResourceImg1"))
+                        {
+                            var file = Request.Form.Files.GetFile("CreateModel.ResourceImg1");
+
+                            string strFilePath = await upload.UploadFileAsync("ResourceImg", file);
+
+                            vm.CreateModel.ResourceImg1 = strFilePath;
+                        }
+                        if (Request.Form.Files[i].Name.Contains("ResourceImg2"))
+                        {
+                            var file = Request.Form.Files.GetFile("CreateModel.ResourceImg2");
+
+                            string strFilePath = await upload.UploadFileAsync("ResourceImg", file);
+
+                            vm.CreateModel.ResourceImg2 = strFilePath;
+                        }
+                        if (Request.Form.Files[i].Name.Contains("ResourceImg3"))
+                        {
+                            var file = Request.Form.Files.GetFile("CreateModel.ResourceImg3");
+
+                            string strFilePath = await upload.UploadFileAsync("ResourceImg", file);
+
+                            vm.CreateModel.ResourceImg3 = strFilePath;
+                        }
+                        if (Request.Form.Files[i].Name.Contains("ResourceImg4"))
+                        {
+                            var file = Request.Form.Files.GetFile("CreateModel.ResourceImg4");
+
+                            string strFilePath = await upload.UploadFileAsync("ResourceImg", file);
+
+                            vm.CreateModel.ResourceImg4 = strFilePath;
+                        }
+                    }
+                }
+                
+                var dbResult = dbAccess.InsertData(vm, LoginUser);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "新增失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "新增失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        [Log(LogActionChineseName.編輯儲存)]
+        [ValidateInput(false)]
+        public async Task<IActionResult> EditOldDataAsync(BorrowMainResourceMangViewModel vm)
+        {
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+
+                var dbResult = dbAccess.UpdateData(vm, LoginUser);
+
+                if (!dbResult.isSuccess)
+                {
+                    dbAccess.DbaRollBack();
+                    vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "修改失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "修改失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        [Log(LogActionChineseName.刪除)]
+        [ValidateInput(false)]
+        public IActionResult Delete(string Ser)
+        {
+            try
+            {
+                dbAccess.DbaInitialTransaction();
+
+                var dbResult = dbAccess.DeletetData(Ser);
+
+                if (!dbResult.isSuccess)
+                {
+                    vmRtn.ErrorCode =  (int)DBActionChineseName.失敗;
+                    vmRtn.ErrorMsg = "刪除失敗";
+                    return Json(vmRtn);
+                }
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "刪除失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+    }
+}
---
+++ PccuClub/DataAccess/BorrowMainResourceMangDataAccess.cs
@@ -0,0 +1,347 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NPOI.POIFS.Crypt;
+using PccuClub.WebAuth;
+using System.Data;
+using System.Text.Encodings.Web;
+using WebPccuClub.Global;
+using WebPccuClub.Global.Extension;
+using WebPccuClub.Models;
+
+namespace WebPccuClub.DataAccess
+{
+    
+    public class BorrowMainResourceMangDataAccess : BaseAccess
+    {
+
+        /// <summary> 查詢結果 </summary>
+
+        public List<BorrowMainResourceMangResultModel> GetSearchResult(BorrowMainResourceMangConditionModel model)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            parameters.Add("@MainClass", model?.MainClass);
+            parameters.Add("@Enable", model?.Enable);
+            parameters.Add("@MainResourceID", model?.MainResourceID);
+            parameters.Add("@MainResourceName", model?.MainResourceName);
+            parameters.Add("@Memo", model?.Memo);
+
+            #region 參數設定
+            #endregion
+
+            CommandText = $@"
+SELECT A.MainResourceID, A.MainResourceName, A.MainClass, B.Text AS MainClassText, A.SecondClass, A.BorrowType, C.Text AS BorrowTypeText, A.ShortDesc, A.BorrowRule, 
+       A.ResourceImg1, A.ResourceImg2, A.ResourceImg3, A.ResourceImg4, 
+       A.AmtReal, A.AmtShelves, A.AmtOnce, A.AmtSafe, A.SafeMessage, A.Enable, D.Text AS EnableText, A.Memo, A.InventoryStatus, 
+       A.Creator, A.Created, A.LastModifier, A.LastModified
+FROM BorrowMainResourceMang A
+LEFT JOIN BorrowMainClassMang B ON B.ID = A.MainClass
+LEFT JOIN Code C ON C.Code = A.BorrowType AND C.Type = 'BorrowMultType'
+LEFT JOIN Code D ON D.Code = A.Enable AND D.Type = 'Enable'
+WHERE 1 = 1
+AND (@MainClass IS NULL OR A.MainClass = @MainClass) 
+AND (@Enable IS NULL OR A.Enable = @Enable) 
+AND (@MainResourceID IS NULL OR A.MainResourceID LIKE '%' + @MainResourceID + '%') 
+AND (@MainResourceName IS NULL OR A.MainResourceName LIKE '%' + @MainResourceName + '%') 
+AND (@Memo IS NULL OR A.Memo LIKE '%' + @Memo + '%') ";
+
+
+            (DbExecuteInfo info, IEnumerable<BorrowMainResourceMangResultModel> entitys) dbResult = DbaExecuteQuery<BorrowMainResourceMangResultModel>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<BorrowMainResourceMangResultModel>();
+        }
+
+        /// <summary>
+        /// 取得編輯資料
+        /// </summary>
+        /// <param name="submitBtn"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public BorrowMainResourceMangEditModel GetEditData(string Ser)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            parameters.Add("@MainResourceID", Ser);
+
+            #region 參數設定
+            #endregion
+
+            CommandText = $@"
+SELECT MainResourceID, MainResourceName, MainClass, SecondClass, BorrowType, ShortDesc, BorrowRule, 
+       ResourceImg1, ResourceImg2, ResourceImg3, ResourceImg4, 
+       AmtReal, AmtShelves, AmtOnce, AmtSafe, SafeMessage, Enable, Memo, InventoryStatus, 
+       Creator, Created, LastModifier, LastModified
+FROM BorrowMainResourceMang
+WHERE 1 = 1
+AND (MainResourceID = @MainResourceID) ";
+
+
+            (DbExecuteInfo info, IEnumerable<BorrowMainResourceMangEditModel> entitys) dbResult = DbaExecuteQuery<BorrowMainResourceMangEditModel>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList().FirstOrDefault();
+
+            return null;
+        }
+
+        /// <summary> 新增資料 </summary>
+        public DbExecuteInfo InsertData(BorrowMainResourceMangViewModel vm, UserInfo LoginUser)
+        {
+
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@MainResourceID", vm.CreateModel.MainResourceID);
+            parameters.Add("@MainResourceName", vm.CreateModel.MainResourceName);
+            parameters.Add("@MainClass", vm.CreateModel.MainClass);
+            parameters.Add("@SecondClass", vm.CreateModel.SecondClass);
+            parameters.Add("@BorrowType", vm.CreateModel.BorrowType);
+            parameters.Add("@ShortDesc", vm.CreateModel.ShortDesc);
+            parameters.Add("@BorrowRule", vm.CreateModel.BorrowRule);
+            parameters.Add("@ResourceImg1", vm.CreateModel.ResourceImg1);
+            parameters.Add("@ResourceImg2", vm.CreateModel.ResourceImg2);
+            parameters.Add("@ResourceImg3", vm.CreateModel.ResourceImg3);
+            parameters.Add("@ResourceImg4", vm.CreateModel.ResourceImg4);
+            parameters.Add("@AmtReal", vm.CreateModel.AmtReal);
+            parameters.Add("@AmtShelves", vm.CreateModel.AmtShelves);
+            parameters.Add("@AmtOnce", vm.CreateModel.AmtOnce);
+            parameters.Add("@AmtSafe", vm.CreateModel.AmtSafe);
+            parameters.Add("@SafeMessage", vm.CreateModel.SafeMessage);
+            parameters.Add("@Enable", vm.CreateModel.Enable);
+            parameters.Add("@Memo", vm.CreateModel.Memo);
+            parameters.Add("@LoginId", LoginUser.LoginId);
+            #endregion 參數設定
+
+            string CommendText = $@"INSERT INTO BorrowMainResourceMang
+                                               (MainResourceID
+                                               ,MainResourceName
+                                               ,MainClass
+                                               ,SecondClass
+                                               ,BorrowType
+                                               ,ShortDesc
+                                               ,BorrowRule
+                                               ,ResourceImg1
+                                               ,ResourceImg2
+                                               ,ResourceImg3
+                                               ,ResourceImg4
+                                               ,AmtReal
+                                               ,AmtShelves
+                                               ,AmtOnce
+                                               ,AmtSafe
+                                               ,SafeMessage
+                                               ,Enable
+                                               ,Memo
+                                               ,InventoryStatus
+                                               ,Creator
+                                               ,Created
+                                               ,LastModifier
+                                               ,LastModified)
+                                         VALUES
+                                               (@MainResourceID
+                                               ,@MainResourceName
+                                               ,@MainClass
+                                               ,@SecondClass
+                                               ,@BorrowType
+                                               ,@ShortDesc
+                                               ,@BorrowRule
+                                               ,@ResourceImg1
+                                               ,@ResourceImg2
+                                               ,@ResourceImg3
+                                               ,@ResourceImg4
+                                               ,@AmtReal
+                                               ,@AmtShelves
+                                               ,@AmtOnce
+                                               ,@AmtSafe
+                                               ,@SafeMessage
+                                               ,@Enable
+                                               ,@Memo
+                                               ,'01'
+                                               ,@LoginId
+                                               ,GETDATE()
+                                               ,@LoginId
+                                               ,GETDATE())";
+
+            ExecuteResult = DbaExecuteNonQuery(CommendText, parameters, false, DBAccessException);
+
+            return ExecuteResult;
+        }
+
+        /// <summary> 修改資料 </summary>
+        public DbExecuteInfo UpdateData(BorrowMainResourceMangViewModel vm, UserInfo LoginUser)
+        {
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@MainResourceID", vm.CreateModel.MainResourceID);
+            parameters.Add("@MainResourceName", vm.CreateModel.MainResourceName);
+            parameters.Add("@MainClass", vm.CreateModel.MainClass);
+            parameters.Add("@SecondClass", vm.CreateModel.SecondClass);
+            parameters.Add("@BorrowType", vm.CreateModel.BorrowType);
+            parameters.Add("@ShortDesc", vm.CreateModel.ShortDesc);
+            parameters.Add("@BorrowRule", vm.CreateModel.BorrowRule);
+            parameters.Add("@ResourceImg1", vm.CreateModel.ResourceImg1);
+            parameters.Add("@ResourceImg2", vm.CreateModel.ResourceImg2);
+            parameters.Add("@ResourceImg3", vm.CreateModel.ResourceImg3);
+            parameters.Add("@ResourceImg4", vm.CreateModel.ResourceImg4);
+            parameters.Add("@AmtReal", vm.CreateModel.AmtReal);
+            parameters.Add("@AmtShelves", vm.CreateModel.AmtShelves);
+            parameters.Add("@AmtOnce", vm.CreateModel.AmtOnce);
+            parameters.Add("@AmtSafe", vm.CreateModel.AmtSafe);
+            parameters.Add("@SafeMessage", vm.CreateModel.SafeMessage);
+            parameters.Add("@Enable", vm.CreateModel.Enable);
+            parameters.Add("@Memo", vm.CreateModel.Memo);
+            parameters.Add("@LoginId", LoginUser.LoginId);
+            #endregion 參數設定
+
+            string CommendText = $@"UPDATE BorrowMainResourceMang 
+                                       SET MainResourceName = @MainResourceName, MainClass = @MainClass, SecondClass = @SecondClass, 
+                                           BorrowType = @BorrowType, ShortDesc = @ShortDesc, BorrowRule = @BorrowRule, 
+                                           ResourceImg1 = @ResourceImg1, ResourceImg2 = @ResourceImg2, ResourceImg3 = @ResourceImg3, ResourceImg4 = @ResourceImg4, 
+                                           AmtReal = @AmtReal, AmtShelves = @AmtShelves, AmtOnce = @AmtOnce, AmtSafe = @AmtSafe, SafeMessage = @SafeMessage, 
+                                           Enable = @Enable, Memo = @Memo, InventoryStatus = @InventoryStatus, 
+                                           LastModifier = @LoginId, LastModified = GETDATE()
+                                     WHERE MainResourceID = @MainResourceID";
+
+            ExecuteResult = DbaExecuteNonQuery(CommendText, parameters, false, DBAccessException);
+
+            return ExecuteResult;
+        }
+
+        /// <summary>
+        /// 刪除資料
+        /// </summary>
+        /// <param name="ser"></param>
+        /// <returns></returns>
+        public DbExecuteInfo DeletetData(string ser)
+        {
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@MainResourceID", ser);
+            #endregion 參數設定
+
+            string CommendText = $@"DELETE FROM BorrowMainResourceMang WHERE MainResourceID = @MainResourceID ";
+
+            ExecuteResult = DbaExecuteNonQuery(CommendText, parameters, false, DBAccessException);
+
+            return ExecuteResult;
+        }
+
+        public DataTable GetMainResourceID(string MainResourceID)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            parameters.Add("@MainResourceID", MainResourceID);
+
+            #region 參數設定
+            #endregion
+
+            CommandText = $@"
+                            SELECT MainResourceID 
+                              FROM BorrowMainResourceMang
+                             WHERE 1 = 1
+                               AND MainResourceID = @MainResourceID";
+
+
+            DbaExecuteQuery(CommandText, parameters, ds, true, DBAccessException);
+            return ds.Tables[0];
+        }
+
+        public List<SelectListItem> GetddlMainClass()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT ID AS Value, Text AS Text FROM BorrowMainClassMang";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+
+        public List<SelectListItem> GetddlSecondClass()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT ID AS Value, Text AS Text FROM BorrowSecondClassMang";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+
+        public List<SelectListItem> GetddlEnable()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT Code AS Value, Text AS Text FROM Code WHERE Type = 'Enable'";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+
+
+        public List<SelectListItem> GetddlBorrowMultType()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT Code AS Value, Text AS Text FROM Code WHERE Type = 'BorrowMultType'";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+    }
+}
