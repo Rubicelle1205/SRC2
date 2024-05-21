77229a652cad5b5b2e805c9c739748a296a5a8fb
Rubicelle <Rubicelle@gmail.com>
2024-05-21 15:57:46 +0800

    [案件] 前台借用類別完成
    Signed-off-by: Rubicelle <Rubicelle@gmail.com>

---
+++ PccuClub/Controllers/FBorrowIndexController.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using PccuClub.WebAuth;
+using System.Web.Mvc;
+using WebPccuClub.DataAccess;
+using WebPccuClub.Global;
+using WebPccuClub.Models;
+
+namespace WebPccuClub.Controllers
+{
+    [LogAttribute(LogActionChineseName.前台借用類別)]
+    public class FBorrowIndexController : FBaseController
+	{
+        ReturnViewModel vmRtn = new ReturnViewModel();
+        FBorrowIndexDataAccess dbAccess = new FBorrowIndexDataAccess();
+
+        [Log(LogActionChineseName.首頁)]
+        public IActionResult Index()
+        {
+            FBorrowIndexViewModel vm = new FBorrowIndexViewModel();
+            vm.ResultModel = dbAccess.GetSearchResult();
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.首頁)]
+        public IActionResult Detail(string submitBtn)
+        {
+            FBorrowIndexViewModel vm = new FBorrowIndexViewModel();
+            vm.Detail = dbAccess.GetEditData(submitBtn);
+            return View(vm);
+        }
+    }
+}
---
+++ PccuClub/Models/FBorrowIndexViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebPccuClub.Models
+{
+    public class FBorrowIndexViewModel
+    {
+		public List<FBorrowIndexResultModel> ResultModel { get; set; }
+
+        public FBorrowIndexDetailModel Detail { get; set; }
+    }
+
+    public class FBorrowIndexResultModel
+	{
+		[DisplayName("ID")]
+		public string? ID { get; set; }
+
+		[DisplayName("分類名稱")]
+        public string? Text { get; set; }
+
+        [DisplayName("封面圖片")]
+        public string? CoverPath { get; set; }
+
+    }
+
+    public class FBorrowIndexDetailModel
+    {
+        [DisplayName("ID")]
+        public string? ID { get; set; }
+
+        [DisplayName("類別名稱")]
+        public string? Text { get; set; }
+
+        [DisplayName("可借用日期")]
+        public DateTime? BorrowSDate { get; set; }
+
+        [DisplayName("可借用日期")]
+        public DateTime? BorrowEDate { get; set; }
+
+        [DisplayName("借用規定")]
+        public string? BorrowRule { get; set; }
+
+        [DisplayName("費用")]
+        public string? BorrowFee { get; set; }
+
+        [DisplayName("庫存機制")]
+        public string? ReserveRule { get; set; }
+
+    }
+}
