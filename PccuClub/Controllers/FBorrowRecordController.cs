5da92ba7a827926f2d9aff8452d115c520d2723b
Rubicelle <Rubicelle@gmail.com>
2024-05-24 23:53:54 +0800

    [前台借用] 暫時上傳
    Signed-off-by: Rubicelle <Rubicelle@gmail.com>

---
+++ PccuClub/Controllers/FBorrowRecordController.cs
@@ -0,0 +1,245 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.ComponentModel;
+using System.Data;
+using System.Reflection;
+using System.Web.Mvc;
+using Utility;
+using WebPccuClub.DataAccess;
+using WebPccuClub.Global;
+using WebPccuClub.Models;
+using static WebPccuClub.Models.FBorrowRecordViewModel;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace WebPccuClub.Controllers
+{
+    [LogAttribute(LogActionChineseName.我的借用紀錄)]
+    public class FBorrowRecordController : FBaseController
+    {
+        ReturnViewModel vmRtn = new ReturnViewModel();
+        FBorrowRecordDataAccess dbAccess = new FBorrowRecordDataAccess();
+        UploadUtil upload = new UploadUtil();
+
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public FBorrowRecordController(IHostingEnvironment _hostingEnvironment)
+        {
+            hostingEnvironment = _hostingEnvironment;
+        }
+
+
+        [Log(LogActionChineseName.首頁)]
+        public IActionResult Index()
+        {
+            
+            ViewBag.ddlMainClass = dbAccess.GetddlMainClass();
+            ViewBag.ddlApplyUnitType = dbAccess.GetddlApplyUnitType();
+            ViewBag.ddlBorrowActVerify = dbAccess.GetddlBorrowActVerify();
+
+            FBorrowRecordViewModel vm = new FBorrowRecordViewModel();
+            vm.ConditionModel = new FBorrowRecordConditionModel();
+            return View(vm);
+        }
+
+        [Log(LogActionChineseName.新增)]
+        public IActionResult Create()
+        {
+            ViewBag.ddlMainClass = dbAccess.GetddlMainClass();
+            ViewBag.ddlApplyUnitType = dbAccess.GetddlApplyUnitType();
+            ViewBag.ddlBorrowActVerify = dbAccess.GetddlBorrowActVerify();
+            ViewBag.ddlSecondResurce = dbAccess.GetddlSecondResurce();
+
+            FBorrowRecordViewModel vm = new FBorrowRecordViewModel();
+            //vm.CreateModel = new FBorrowRecordCreateModel();
+            return View(vm);
+        }
+
+
+        [LogAttribute(LogActionChineseName.查詢)]
+        public IActionResult GetSearchResult(FBorrowRecordViewModel vm)
+        {
+            vm.ResultModel = dbAccess.GetSearchResult(LoginUser).ToList();
+
+            #region 分頁
+            vm.ConditionModel.TotalCount = vm.ResultModel.Count();
+            int StartRow = vm.ConditionModel.Page * vm.ConditionModel.PageSize;
+            vm.ResultModel = vm.ResultModel.Skip(StartRow).Take(vm.ConditionModel.PageSize).ToList();
+            #endregion
+
+            return PartialView("_SearchResultPartial", vm);
+        }
+
+        [Log(LogActionChineseName.新增儲存)]
+        [ValidateInput(false)]
+        public async Task<IActionResult> SaveNewDataAsync(FBorrowRecordViewModel vm)
+        {
+            try
+            {
+                //dbAccess.DbaInitialTransaction();
+
+                //if (Request.Form.Files.Count > 0)
+                //{
+                //    for (int i = 0; i <= Request.Form.Files.Count - 1; i++)
+                //    {
+                //        if (Request.Form.Files[i].Name.Contains("File"))
+                //        {
+                //            var file = Request.Form.Files[i];
+
+                //            string strFilePath = await upload.UploadFileAsync("BorrowRecord", file);
+
+                //            FBorrowRecordFileModel model = new FBorrowRecordFileModel();
+                //            model.FileName = file.FileName;
+                //            model.FilePath = strFilePath;
+
+                //            vm.CreateModel.LstFile.Add(model);
+                //        }
+                //    }
+                //}
+
+                //List<string> LstMainResourceID = new List<string>();
+                //string[] arr = vm.CreateModel.strDeviceData.Split("|");
+
+                //for (int i = 0; i <= arr.Length - 1; i++)
+                //{
+                //    string [] arrData = arr[i].Split(",");
+
+                //    string Device = arrData[0];
+                //    string Amt = arrData[1];
+
+                //    DataTable dt = dbAccess.GetMainResourceID(Device);
+
+                //    string MainResourceID = dt.QueryFieldByDT("MainClass");
+                //    string BorrowType = dt.QueryFieldByDT("BorrowType");
+
+                //    LstMainResourceID.Add(MainResourceID);
+
+                //    FBorrowRecordDeviceModel DeviceModel = new FBorrowRecordDeviceModel();
+                    
+                //    //非大量借用
+                //    if (BorrowType == "02")
+                //    {
+                //        int TotAmt = Int32.Parse(Amt);
+
+                //        for (int j = 1; j <= TotAmt; j++)
+                //        {
+                //            DeviceModel.MainResourceID = MainResourceID;
+                //            DeviceModel.SecondResourceNo = Device;
+                //            DeviceModel.BorrowAmt = "1";
+                //            DeviceModel.BorrowStatus = "01";
+                //            vm.CreateModel.LstDevice.Add(DeviceModel);
+                //        }
+                //    }
+                //    else
+                //    {
+                //        DeviceModel.MainResourceID = MainResourceID;
+                //        DeviceModel.SecondResourceNo = Device;
+                //        DeviceModel.BorrowAmt = Amt;
+                //        DeviceModel.BorrowStatus = "01";
+                //        vm.CreateModel.LstDevice.Add(DeviceModel);
+                //    }
+                //}
+
+                //for (int i = 0; i <= LstMainResourceID.Count - 1; i++)
+                //{
+                //    DataTable dt = new DataTable();
+
+                //    vm.CreateModel.MainClassID = LstMainResourceID[i];
+                //    var dbResult = dbAccess.InsertMainData(vm, LoginUser, out dt);
+
+                //    if (!dbResult.isSuccess)
+                //    {
+                //        dbAccess.DbaRollBack();
+                //        vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                //        vmRtn.ErrorMsg = "新增失敗";
+                //        return Json(vmRtn);
+                //    }
+
+                //    string BorrowMainID = dt.QueryFieldByDT("BorrowMainID");
+
+
+                //    List<FBorrowRecordDeviceModel> datalist = vm.CreateModel.LstDevice.Where(x => x.MainResourceID == LstMainResourceID[i]).ToList();
+                //    dbResult = dbAccess.InsertDeviceData(datalist, LoginUser, BorrowMainID);
+
+                //    if (!dbResult.isSuccess)
+                //    {
+                //        dbAccess.DbaRollBack();
+                //        vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                //        vmRtn.ErrorMsg = "新增失敗";
+                //        return Json(vmRtn);
+                //    }
+
+                //    if (vm.CreateModel.LstFile.Count > 0)
+                //    {
+                //        dbResult = dbAccess.InsertFileData(vm.CreateModel.LstFile, LoginUser, BorrowMainID);
+
+                //        if (!dbResult.isSuccess)
+                //        {
+                //            dbAccess.DbaRollBack();
+                //            vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                //            vmRtn.ErrorMsg = "新增失敗";
+                //            return Json(vmRtn);
+                //        }
+                //    }
+                    
+                //}
+
+                dbAccess.DbaCommit();
+            }
+            catch (Exception ex)
+            {
+                dbAccess.DbaRollBack();
+                vmRtn.ErrorCode = (int)DBActionChineseName.失敗;
+                vmRtn.ErrorMsg = "新增失敗" + ex.Message;
+                return Json(vmRtn);
+            }
+
+            return Json(vmRtn);
+        }
+
+        [Log(LogActionChineseName.取得上架數量)]
+        [ValidateInput(false)]
+        public IActionResult InitBorrowAmt(string MainResourceID)
+        {
+            if (!string.IsNullOrEmpty(MainResourceID))
+            {
+                ViewBag.ddlSecondResurce = dbAccess.GetddlSecondResurce();
+            }
+
+            DataTable dt = new DataTable();
+
+            dbAccess.GetBorrowAmt(MainResourceID, out dt);
+
+            string AmtShelves = dt.QueryFieldByDT("AmtShelves");
+            string AmtOnce = dt.QueryFieldByDT("AmtOnce");
+
+            FBorrowRecordViewModel vm = new FBorrowRecordViewModel();
+            //vm.CreateModel = new FBorrowRecordCreateModel();
+            vm.CreateModel.MainResourceID = MainResourceID;
+            vm.CreateModel.AmtShelves = AmtShelves;
+            vm.CreateModel.AmtOnce = AmtOnce;
+
+
+            List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem> LstItem = new List<Microsoft.AspNetCore.Mvc.Rendering.SelectListItem>();
+
+            for (int i = 1; i <= Int32.Parse(AmtShelves); i++)
+            {
+                if (i > Int32.Parse(AmtOnce))
+                {
+                    LstItem.Add(new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem() { Value = i.ToString(), Text = string.Format("{0}", i), Disabled = true });
+                }
+                else
+                {
+                    LstItem.Add(new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem() { Value = i.ToString(), Text = string.Format("{0}", i)});
+                }
+                
+            }
+
+            ViewBag.ddlSecondAmt = LstItem;
+
+            return PartialView("_BorrowAmtPartial", vm);
+        }
+
+    }
+}
---
+++ PccuClub/DataAccess/FBorrowRecordDataAccess.cs
@@ -0,0 +1,369 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NPOI.POIFS.Crypt;
+using PccuClub.WebAuth;
+using System.Data;
+using System.Text.Encodings.Web;
+using WebPccuClub.Global;
+using WebPccuClub.Global.Extension;
+using WebPccuClub.Models;
+using static WebPccuClub.Models.FBorrowRecordViewModel;
+
+namespace WebPccuClub.DataAccess
+{
+    
+    public class FBorrowRecordDataAccess : BaseAccess
+    {
+
+        /// <summary> 查詢結果 </summary>
+
+        public List<FBorrowRecordResultModel> GetSearchResult(UserInfo LoginUser)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+
+            #region 參數設定
+
+            parameters.Add("@Creator", LoginUser.LoginId);
+
+            #endregion
+
+            CommandText = $@"SELECT A.BorrowMainID, A.ActVerify, B.Text AS ActVerifyText, A.ActName, A.Created
+                               FROM BorrowMain A
+                          LEFT JOIN Code B ON B.Code = A.ActVerify AND B.Type = 'BorrowActVerify'
+WHERE 1 = 1
+AND Creator = @Creator";
+
+
+            (DbExecuteInfo info, IEnumerable<FBorrowRecordResultModel> entitys) dbResult = DbaExecuteQuery<FBorrowRecordResultModel>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<FBorrowRecordResultModel>();
+        }
+
+
+        #region 新增
+
+        /// <summary> 新增資料 </summary>
+        public DbExecuteInfo InsertMainData(FBorrowRecordViewModel vm, UserInfo LoginUser, out DataTable dt)
+        {
+            DataSet ds = new DataSet();
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@MainClassID", vm.CreateModel.MainClassID);
+            parameters.Add("@ApplyUnitType", vm.CreateModel.ApplyUnitType);
+            parameters.Add("@ApplyUnitName", vm.CreateModel.ApplyUnitName);
+            parameters.Add("@ApplyMan", vm.CreateModel.ApplyMan);
+            parameters.Add("@ApplyTitle", vm.CreateModel.ApplyTitle);
+            parameters.Add("@ApplyEmail", vm.CreateModel.ApplyEmail);
+            parameters.Add("@ApplyTel", vm.CreateModel.ApplyTel);
+            parameters.Add("@ApplyPurpose", vm.CreateModel.ApplyPurpose);
+            parameters.Add("@ActName", vm.CreateModel.ActName);
+            parameters.Add("@UseLocation", vm.CreateModel.UseLocation);
+            parameters.Add("@UseDesc", vm.CreateModel.UseDesc);
+            parameters.Add("@UseSDate", vm.CreateModel.UseSDate);
+            parameters.Add("@UseEDate", vm.CreateModel.UseEDate);
+            parameters.Add("@TakeSDate", vm.CreateModel.TakeSDate);
+            parameters.Add("@TakeEDate", vm.CreateModel.TakeEDate);
+            parameters.Add("@BorrowMemo", vm.CreateModel.BorrowMemo);
+            parameters.Add("@TeacherMark", vm.CreateModel.TeacherMark);
+            parameters.Add("@DeviceMark", vm.CreateModel.DeviceMark);
+            parameters.Add("@TakeMark", vm.CreateModel.TakeMark);
+            parameters.Add("@ReturnMark", vm.CreateModel.ReturnMark);
+            parameters.Add("@ActVerify", vm.CreateModel.ActVerify);
+            parameters.Add("@LoginId", LoginUser.LoginId);
+            #endregion 參數設定
+
+            string CommendText = $@"INSERT INTO BorrowMain
+                                               (MainClassID, 
+                                                ApplyUnitType, 
+                                                ApplyUnitName, 
+                                                ApplyMan, 
+                                                ApplyTitle, 
+                                                ApplyEmail, 
+                                                ApplyTel, 
+                                                ApplyPurpose, 
+                                                ActName, 
+                                                UseLocation, 
+                                                UseDesc, 
+                                                UseSDate, 
+                                                UseEDate, 
+                                                TakeSDate, 
+                                                TakeEDate, 
+                                                BorrowMemo, 
+                                                TeacherMark, 
+                                                DeviceMark, 
+                                                TakeMark, 
+                                                ReturnMark, 
+                                                ActVerify, 
+                                                Creator, 
+                                                Created, 
+                                                LastModifier, 
+                                                LastModified )
+                                         OUTPUT Inserted.BorrowMainID
+                                         VALUES
+                                               (@MainClassID, 
+                                                @ApplyUnitType, 
+                                                @ApplyUnitName, 
+                                                @ApplyMan, 
+                                                @ApplyTitle, 
+                                                @ApplyEmail, 
+                                                @ApplyTel, 
+                                                @ApplyPurpose, 
+                                                @ActName, 
+                                                @UseLocation, 
+                                                @UseDesc, 
+                                                @UseSDate, 
+                                                @UseEDate, 
+                                                @TakeSDate, 
+                                                @TakeEDate, 
+                                                @BorrowMemo, 
+                                                @TeacherMark, 
+                                                @DeviceMark, 
+                                                @TakeMark, 
+                                                @ReturnMark, 
+                                                @ActVerify, 
+                                                @LoginId, 
+                                                GETDATE(), 
+                                                @LoginId, 
+                                                GETDATE() )";
+
+            ExecuteResult = DbaExecuteQuery(CommendText, parameters, ds, true, DBAccessException);
+
+            dt = ds.Tables[0];
+
+            return ExecuteResult;
+        }
+
+        public DbExecuteInfo InsertDeviceData(List<FBorrowRecordDeviceModel> dataList, UserInfo loginUser, string BorrowMainID)
+        {
+
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            string CommendText = $@"INSERT INTO BorrowDevice
+                                            (BorrowMainID
+                                            ,MainResourceID
+                                            ,SecondResourceNo
+                                            ,BorrowStatus
+                                            ,BorrowAmt
+                                            ,Creator
+                                            ,Created
+                                            ,LastModifier
+                                            ,LastModified)
+                                        VALUES
+                                            ('{BorrowMainID}'
+                                            ,@MainResourceID
+                                            ,@SecondResourceNo
+                                            ,@BorrowStatus
+                                            ,@BorrowAmt
+                                            ,'{loginUser.LoginId}'
+                                            ,GETDATE()
+                                            ,'{loginUser.LoginId}'
+                                            ,GETDATE() )";
+
+            ExecuteResult = DbaExecuteNonQueryWithBulk(CommendText, dataList, false, DBAccessException, null);
+
+            return ExecuteResult;
+        }
+
+        public DbExecuteInfo InsertFileData(List<FBorrowRecordFileModel> dataList, UserInfo loginUser, string BorrowMainID)
+        {
+
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            string CommendText = $@"INSERT INTO BorrowFile
+                                            (BorrowMainID
+                                            ,FileName
+                                            ,FilePath
+                                            ,Creator
+                                            ,Created
+                                            ,LastModifier
+                                            ,LastModified)
+                                        VALUES
+                                            ('{BorrowMainID}'
+                                            ,@FileName
+                                            ,@FilePath
+                                            ,'{loginUser.LoginId}'
+                                            ,GETDATE()
+                                            ,'{loginUser.LoginId}'
+                                            ,GETDATE() )";
+
+            ExecuteResult = DbaExecuteNonQueryWithBulk(CommendText, dataList, false, DBAccessException, null);
+
+            return ExecuteResult;
+        }
+
+        /// <summary> 新增事件原因及經過資料 </summary>
+        public DbExecuteInfo InsertEventData(List<EventData> dataList, UserInfo loginUser, string BorrowMainID)
+        {
+
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            string CommendText = $@"INSERT INTO BorrowHistroy
+                                            (BorrowMainID
+                                            ,EventID
+                                            ,EventDateTime
+                                            ,Text
+                                            ,Creator
+                                            ,Created
+                                            ,LastModifier
+                                            ,LastModified)
+                                        VALUES
+                                            ('{BorrowMainID}'
+                                            ,@EventID
+                                            ,@EventDateTime
+                                            ,@Text
+                                            ,'{loginUser.LoginId}'
+                                            ,GETDATE()
+                                            ,'{loginUser.LoginId}'
+                                            ,GETDATE() )";
+
+            ExecuteResult = DbaExecuteNonQueryWithBulk(CommendText, dataList, false, DBAccessException, null);
+
+            return ExecuteResult;
+        }
+
+        #endregion
+
+
+        public List<SelectListItem> GetddlMainClass()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT ID AS Value, Text AS Text FROM BorrowMainClassMang";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+
+        public List<SelectListItem> GetddlBorrowActVerify()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT Code AS Value, Text AS Text FROM Code WHERE Type like 'BorrowActVerify'";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+
+        public List<SelectListItem> GetddlApplyUnitType()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT Code AS Value, Text AS Text FROM Code WHERE Type like 'ApplyUnitType'";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+
+        public List<SelectListItem> GetddlSecondResurce()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT A.MainResourceID AS VALUE, '(' + B.Text + ')' + A.MainResourceName AS TEXT
+                              FROM BorrowMainResourceMang A
+                         LEFT JOIN BorrowMainClassMang B ON B.ID = A.MainClass";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+
+        public DataTable GetBorrowAmt(string MainResourceID, out DataTable dt)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@MainResourceID", MainResourceID);
+            #endregion
+
+            CommandText = @"SELECT MainResourceID, AmtShelves, AmtOnce
+                              FROM BorrowMainResourceMang
+                             WHERE 1 = 1
+                               AND MainResourceID = @MainResourceID ";
+
+            DbaExecuteQuery(CommandText, parameters, ds, true, DBAccessException);
+
+            dt = ds.Tables[0];
+
+            return dt;
+        }
+
+        public DataTable GetMainResourceID(string SecondResourceID)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@SecondResourceID", SecondResourceID);
+            #endregion
+
+            CommandText = @"SELECT MainClass, BorrowType
+                              FROM BorrowMainResourceMang
+                             WHERE 1 = 1
+                               AND MainResourceID = @SecondResourceID ";
+
+            DbaExecuteQuery(CommandText, parameters, ds, true, DBAccessException);
+
+            return ds.Tables[0];
+        }
+
+
+
+    }
+}
