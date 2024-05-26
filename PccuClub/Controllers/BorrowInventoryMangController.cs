bcd0bdcc258b45a52c4aea67905caf7cc8ac9332
Rubicelle <Rubicelle@gmail.com>
2024-05-26 23:37:12 +0800

    暫時上傳
    Signed-off-by: Rubicelle <Rubicelle@gmail.com>

---
+++ PccuClub/Controllers/BorrowInventoryMangController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Web.Mvc;
+using Utility;
+using WebPccuClub.DataAccess;
+using WebPccuClub.Global;
+using WebPccuClub.Models;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace WebPccuClub.Controllers
+{
+    [LogAttribute(LogActionChineseName.業務分類維護)]
+    public class BorrowInventoryMangController : BaseController
+    {
+        ReturnViewModel vmRtn = new ReturnViewModel();
+        //BorrowInventoryMangDataAccess dbAccess = new BorrowInventoryMangDataAccess();
+        UploadUtil upload = new UploadUtil();
+
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public BorrowInventoryMangController(IHostingEnvironment _hostingEnvironment)
+        {
+            hostingEnvironment = _hostingEnvironment;
+        }
+
+
+        [Log(LogActionChineseName.首頁)]
+        public IActionResult Index()
+        {
+            //BorrowInventoryMangViewModel vm = new BorrowInventoryMangViewModel();
+            //vm.ConditionModel = new BorrowInventoryMangConditionModel();
+            return View();
+        }
+    }
+}
---
+++ PccuClub/Controllers/FResourceBorrowController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.Formula.Functions;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Web.Mvc;
+using Utility;
+using WebPccuClub.DataAccess;
+using WebPccuClub.Global;
+using WebPccuClub.Models;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace WebPccuClub.Controllers
+{
+    [LogAttribute(LogActionChineseName.業務分類維護)]
+    public class FResourceBorrowController : FBaseController
+    {
+        ReturnViewModel vmRtn = new ReturnViewModel();
+        //FResourceBorrowDataAccess dbAccess = new FResourceBorrowDataAccess();
+        UploadUtil upload = new UploadUtil();
+
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public FResourceBorrowController(IHostingEnvironment _hostingEnvironment)
+        {
+            hostingEnvironment = _hostingEnvironment;
+        }
+
+
+        [Log(LogActionChineseName.首頁)]
+        public IActionResult Index()
+        {
+            //FResourceBorrowViewModel vm = new FResourceBorrowViewModel();
+            //vm.ConditionModel = new FResourceBorrowConditionModel();
+            return View();
+        }
+    }
+}
