53ef08cc0abfaf6ecf8d1fcfd8f0c4ad28333138
MSI\Pu <Rubicelle1205@gmail.com>
2025-04-12 15:43:44 +0800

    2-2 [後台] 社團評鑑計分維護
    Signed-off-by: MSI\Pu <Rubicelle1205@gmail.com>

---
+++ PccuClub/DataAccess/ClubEvaluationMangDataAccess.cs
@@ -0,0 +1,230 @@
+﻿using DataAccess;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NPOI.POIFS.Crypt;
+using PccuClub.WebAuth;
+using System.Data;
+using System.Text.Encodings.Web;
+using WebPccuClub.Global;
+using WebPccuClub.Global.Extension;
+using WebPccuClub.Models;
+
+namespace WebPccuClub.DataAccess
+{
+    
+    public class ClubEvaluationMangDataAccess : BaseAccess
+    {
+
+        /// <summary> 查詢結果 </summary>
+        public List<ClubEvaluationMangResultModel> GetSearchResult(ClubEvaluationMangConditionModel model)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+			#region 參數設定
+			
+            parameters.Add("@SchoolYear", model?.SchoolYear);
+			parameters.Add("@ClubID", model?.ClubID);
+            parameters.Add("@ClubCName", model?.ClubCName);
+            parameters.Add("@Memo", model?.Memo);
+            parameters.Add("@ClubEvaluationClassId", model?.ClubEvaluationClassId);
+            parameters.Add("@ClubEvaluationItemId", model?.ClubEvaluationItemId);
+
+            parameters.Add("@FromDate", model.From_ReleaseDate.HasValue ? model.From_ReleaseDate.Value.ToString("yyyy/MM/dd 00:00:00") : null);
+            parameters.Add("@ToDate", model.To_ReleaseDate.HasValue ? model.To_ReleaseDate.Value.ToString("yyyy/MM/dd 23:59:59") : null);
+     
+            #endregion
+
+            CommandText = $@"SELECT A.ClubEvaluationId, A.SchoolYear, A.ClubID, A.ClubEvaluationItemId, A.Score, A.Memo, 
+                                    B.ClubCName, D.ClassName, C.ItemName, A.Creator, A.Created, A.LastModifier, A.LastModified
+                               FROM ClubEvaluationMang A
+                          LEFT JOIN ClubMang B ON B.ClubID = A.ClubID 
+                          LEFT JOIN ClubEvaluationItemMang C ON C.ClubEvaluationItemId = A.ClubEvaluationItemId 
+                          LEFT JOIN ClubEvaluationClassMang D ON D.ClubEvaluationClassId = A.ClubEvaluationClassId 
+                              WHERE 1 = 1
+{(model.From_ReleaseDate.HasValue && model.To_ReleaseDate.HasValue ? " AND A.Created BETWEEN @FromDate AND @ToDate" : " ")}
+AND (@SchoolYear IS NULL OR A.SchoolYear LIKE '%' + @SchoolYear + '%') 
+AND (@ClubID IS NULL OR A.ClubID LIKE '%' + @ClubID + '%') 
+AND (@ClubCName IS NULL OR B.ClubCName LIKE '%' + @ClubCName + '%') 
+AND (@Memo IS NULL OR A.Memo LIKE '%' + @Memo + '%') 
+AND (@ClubEvaluationItemId IS NULL OR C.ClubEvaluationItemId LIKE '%' + @ClubEvaluationItemId + '%') 
+AND (@ClubEvaluationClassId IS NULL OR D.ClubEvaluationClassId LIKE '%' + @ClubEvaluationClassId + '%') ";
+
+
+            (DbExecuteInfo info, IEnumerable<ClubEvaluationMangResultModel> entitys) dbResult = DbaExecuteQuery<ClubEvaluationMangResultModel>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<ClubEvaluationMangResultModel>();
+        }
+
+        /// <summary> 取得編輯資料 </summary>
+        public ClubEvaluationMangEditModel GetEditData(string Ser)
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+			#region 參數設定
+
+			parameters.Add("@ID", Ser);
+
+            #endregion
+
+            CommandText = $@"SELECT ClubEvaluationId, SchoolYear, ClubID, ClubEvaluationClassId, ClubEvaluationItemId, 
+                                    Score, Memo, Creator, Created, LastModifier, LastModified
+                               FROM ClubEvaluationMang
+                              WHERE 1 = 1
+                                AND (ClubEvaluationId = @ID) ";
+
+
+            (DbExecuteInfo info, IEnumerable<ClubEvaluationMangEditModel> entitys) dbResult = DbaExecuteQuery<ClubEvaluationMangEditModel>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList().FirstOrDefault();
+
+            return null;
+        }
+
+        /// <summary> 新增資料 </summary>
+        public DbExecuteInfo InsertData(ClubEvaluationMangViewModel vm, UserInfo LoginUser)
+        {
+
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+			#region 參數設定
+			parameters.Add("@SchoolYear", vm.CreateModel.SchoolYear);
+            parameters.Add("@ClubID", vm.CreateModel.ClubID);
+            parameters.Add("@ClubEvaluationClassId", vm.CreateModel.ClubEvaluationClassId);
+            parameters.Add("@ClubEvaluationItemId", vm.CreateModel.ClubEvaluationItemId);
+			parameters.Add("@Score", vm.CreateModel.Score);
+            parameters.Add("@Memo", vm.CreateModel.Memo);
+            parameters.Add("@LoginId", LoginUser.LoginId);
+            #endregion 參數設定
+
+            string CommendText = $@"INSERT INTO ClubEvaluationMang
+                                               (SchoolYear
+                                               ,ClubID
+                                               ,ClubEvaluationClassId
+                                               ,ClubEvaluationItemId
+                                               ,Score
+                                               ,Memo
+                                               ,Creator
+                                               ,Created
+                                               ,LastModifier
+                                               ,LastModified)
+                                         VALUES
+                                               (@SchoolYear
+                                               ,@ClubID
+                                               ,@ClubEvaluationClassId
+                                               ,@ClubEvaluationItemId
+                                               ,@Score
+                                               ,@Memo
+                                               ,@LoginId
+                                               ,GETDATE()
+                                               ,@LoginId
+                                               ,GETDATE())";
+
+            ExecuteResult = DbaExecuteNonQuery(CommendText, parameters, false, DBAccessException);
+
+            return ExecuteResult;
+        }
+
+        /// <summary> 修改資料 </summary>
+        public DbExecuteInfo UpdateData(ClubEvaluationMangViewModel vm, UserInfo LoginUser)
+        {
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@ClubEvaluationId", vm.EditModel.ClubEvaluationId);
+			parameters.Add("@SchoolYear", vm.EditModel.SchoolYear);
+            parameters.Add("@ClubID", vm.EditModel.ClubID);
+            parameters.Add("@ClubEvaluationClassId", vm.EditModel.ClubEvaluationClassId);
+            parameters.Add("@ClubEvaluationItemId", vm.EditModel.ClubEvaluationItemId);
+			parameters.Add("@Score", vm.EditModel.Score);
+			parameters.Add("@Memo", vm.EditModel.Memo);
+            parameters.Add("@LoginId", LoginUser.LoginId);
+            #endregion 參數設定
+
+            string CommendText = $@"UPDATE ClubEvaluationMang 
+                                       SET SchoolYear = @SchoolYear, ClubEvaluationClassId = @ClubEvaluationClassId, ClubEvaluationItemId = @ClubEvaluationItemId, 
+                                           ClubID = @ClubID, Score = @Score, Memo = @Memo, LastModifier = @LoginId, LastModified = GETDATE()
+                                     WHERE ClubEvaluationId = @ClubEvaluationId";
+
+            ExecuteResult = DbaExecuteNonQuery(CommendText, parameters, false, DBAccessException);
+
+            return ExecuteResult;
+        }
+
+        /// <summary>
+        /// 刪除資料
+        /// </summary>
+        /// <param name="ser"></param>
+        /// <returns></returns>
+        public DbExecuteInfo DeletetData(string ser)
+        {
+            DbExecuteInfo ExecuteResult = new DbExecuteInfo();
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            parameters.Add("@ID", ser);
+            #endregion 參數設定
+
+            string CommendText = $@"DELETE FROM ClubEvaluationMang WHERE ClubEvaluationId = @ID";
+
+            ExecuteResult = DbaExecuteNonQuery(CommendText, parameters, false, DBAccessException);
+
+            return ExecuteResult;
+        }
+
+        public List<SelectListItem> GetClassId()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT A.ClubEvaluationClassId AS VALUE, A.ClassName AS Text 
+							  FROM ClubEvaluationClassMang A 
+";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+
+        public List<SelectListItem> GetItemId()
+        {
+            string CommandText = string.Empty;
+            DataSet ds = new DataSet();
+
+            DBAParameter parameters = new DBAParameter();
+
+            #region 參數設定
+            #endregion
+
+            CommandText = @"SELECT A.ClubEvaluationItemId AS VALUE, A.ItemName AS Text 
+							  FROM ClubEvaluationItemMang A 
+";
+
+            (DbExecuteInfo info, IEnumerable<SelectListItem> entitys) dbResult = DbaExecuteQuery<SelectListItem>(CommandText, parameters, true, DBAccessException);
+
+            if (dbResult.info.isSuccess && dbResult.entitys.Count() > 0)
+                return dbResult.entitys.ToList();
+
+            return new List<SelectListItem>();
+        }
+    }
+}
---
+++ PccuClub/Models/ClubEvaluationMangViewModel.cs
@@ -0,0 +1,180 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebPccuClub.Models
+{
+    public class ClubEvaluationMangViewModel
+    {
+        [DisplayName("檔案上傳")]
+        [UIHint("_UploadFile")]
+        public IFormFile? File { get; set; }
+
+        public ClubEvaluationMangConditionModel ConditionModel { get; set; }
+
+        public List<ClubEvaluationMangResultModel> ResultModel { get; set; }
+
+        public ClubEvaluationMangCreateModel CreateModel { get; set; }
+
+        public ClubEvaluationMangEditModel EditModel { get; set; }
+
+    }
+
+    public class ClubEvaluationMangConditionModel
+    {
+        public ClubEvaluationMangConditionModel()
+        {
+            this.Page = 0;
+            this.PageSize = 10;
+            this.TotalCount = 0;
+        }
+
+        /// <summary> 目前頁數 </summary>
+        public int Page { get; set; }
+
+        /// <summary> 預設每頁顯示筆數 - 依需求更改 </summary>
+        public int PageSize { get; set; }
+
+        /// <summary> 總筆數 </summary>
+        public int TotalCount { get; set; }
+
+		/// <summary>學年度</summary>
+		[DisplayName("學年度")]
+		public string? SchoolYear { get; set; }
+
+        /// <summary>社團</summary>
+        [DisplayName("社團")]
+        public string? ClubID { get; set; }
+
+        /// <summary>社團名稱</summary>
+        [DisplayName("社團名稱")]
+        public string? ClubCName { get; set; }
+
+        /// <summary>備註</summary>
+        [DisplayName("備註")]
+        public string? Memo { get; set; }
+
+        [DisplayName("起始日期")]
+        public DateTime? From_ReleaseDate { get; set; }
+
+        [DisplayName("結束日期")]
+        public DateTime? To_ReleaseDate { get; set; }
+
+        /// <summary>評鑑類別</summary>
+        [DisplayName("評鑑類別")]
+        public string? ClubEvaluationClassId { get; set; }
+
+        /// <summary>評鑑項目</summary>
+        [DisplayName("評鑑項目")]
+        public string? ClubEvaluationItemId { get; set; }
+
+    }
+
+    public class ClubEvaluationMangResultModel
+    {
+
+        public int? ClubEvaluationId { get; set; }
+
+        /// <summary>學年度</summary>
+        [DisplayName("學年度")]
+		public string? SchoolYear { get; set; }
+
+        /// <summary>社團</summary>
+        [DisplayName("社團")]
+        public string? ClubID { get; set; }
+
+        /// <summary>社團名稱</summary>
+        [DisplayName("社團名稱")]
+        public string? ClubCName { get; set; }
+
+        /// <summary>評鑑類別</summary>
+        [DisplayName("評鑑類別")]
+        public string? ClubEvaluationClassId { get; set; }
+
+        /// <summary>評鑑項目</summary>
+        [DisplayName("評鑑項目")]
+        public string? ClubEvaluationItemId { get; set; }
+
+        /// <summary>評鑑類別</summary>
+        [DisplayName("評鑑類別")]
+        public string? ClassName { get; set; }
+
+        /// <summary>評鑑項目</summary>
+        [DisplayName("評鑑項目")]
+        public string? ItemName { get; set; }
+
+        /// <summary>分數</summary>
+        [DisplayName("分數")]
+        public string? Score { get; set; }
+
+        /// <summary>備註</summary>
+        [DisplayName("備註")]
+        public string? Memo { get; set; }
+
+        [DisplayName("建立時間")]
+        public DateTime? Created { get; set; }
+    }
+
+    public class ClubEvaluationMangCreateModel
+    {
+		/// <summary>學年度</summary>
+		[DisplayName("學年度")]
+		public string? SchoolYear { get; set; }
+
+        /// <summary>社團</summary>
+        [DisplayName("社團")]
+        public string? ClubID { get; set; }
+
+        /// <summary>評鑑類別</summary>
+        [DisplayName("評鑑類別")]
+        public string? ClubEvaluationClassId { get; set; }
+
+        /// <summary>評鑑項目</summary>
+        [DisplayName("評鑑項目")]
+        public string? ClubEvaluationItemId { get; set; }
+
+        /// <summary>分數</summary>
+        [DisplayName("分數")]
+        public string? Score { get; set; }
+
+        /// <summary>備註</summary>
+        [DisplayName("備註")]
+        public string? Memo { get; set; }
+    }
+
+    public class ClubEvaluationMangEditModel
+    {
+        public int? ClubEvaluationId { get; set; }
+
+		/// <summary>學年度</summary>
+		[DisplayName("學年度")]
+		public string? SchoolYear { get; set; }
+
+        /// <summary>社團</summary>
+        [DisplayName("社團")]
+        public string? ClubID { get; set; }
+
+        /// <summary>評鑑類別</summary>
+        [DisplayName("評鑑類別")]
+        public string? ClubEvaluationClassId { get; set; }
+
+        /// <summary>評鑑項目</summary>
+        [DisplayName("評鑑項目")]
+        public string? ClubEvaluationItemId { get; set; }
+
+        /// <summary>分數</summary>
+        [DisplayName("分數")]
+        public string? Score { get; set; }
+
+        /// <summary>備註</summary>
+        [DisplayName("備註")]
+        public string? Memo { get; set; }
+
+        /// <summary>建立時間</summary>
+        [DisplayName("建立時間")]
+        public DateTime? Created { get; set; }
+
+        /// <summary>更新時間</summary>
+        [DisplayName("更新時間")]
+        public DateTime? LastModified { get; set; }
+    }
+}
