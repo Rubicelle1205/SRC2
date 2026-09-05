9de9ff905ff6079ac1bddaaab39786b6c5b696ba
MSI\Pu <Rubicelle1205@gmail.com>
2026-09-05 16:36:28 +0800

    [會員名冊] 增加參與期間起訖功能
    Signed-off-by: MSI\Pu <Rubicelle1205@gmail.com>

--- PccuClub/Models/MemberMangViewModel.cs
+++ PccuClub/Models/MemberMangViewModel.cs
@@ -72,8 +72,11 @@ namespace WebPccuClub.Models
         public DateTime? To_ReleaseDate { get; set; }
 
         [DisplayName("參與期間")]
-        public DateTime? DuringDate { get; set; }
-        
+        public DateTime? From_DuringDate { get; set; }
+
+        [DisplayName("參與期間")]
+        public DateTime? To_DuringDate { get; set; }
+
     }
 
     public class MemberMangResultModel
